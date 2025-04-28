80b6cbc44677b869190a922f3e926ead4d9178ee
William Dias <smdwilliam89@gmail.com>
2025-04-28 11:01:56 -0300

    feat(script): Avançando com o from.

--- script.js
+++ script.js
@@ -1,4 +1,10 @@
-gsap.to('.quadrado', {
+// gsap.to('.quadrado', {
+//     x: '200',
+//     duration: 3,
+//     backgroundColor: 'pink',
+// });
+
+gsap.from('.quadrado', {
     x: '200',
     duration: 3,
     backgroundColor: 'pink',
