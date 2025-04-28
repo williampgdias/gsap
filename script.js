44e387a4b0e66fcdb99da4c75b32dd76ff566515
William Dias <smdwilliam89@gmail.com>
2025-04-28 11:38:42 -0300

    feat(script): Trabalhando com o fromTo.

--- script.js
+++ script.js
@@ -4,8 +4,22 @@
 //     backgroundColor: 'pink',
 // });
 
-gsap.from('.quadrado', {
-    x: '200',
-    duration: 3,
-    backgroundColor: 'pink',
-});
+// gsap.from('.quadrado', {
+//     x: '200',
+//     duration: 3,
+//     backgroundColor: 'pink',
+// });
+
+gsap.fromTo(
+    '.quadrado',
+    {
+        x: -200,
+        y: 100,
+    },
+    {
+        x: 100,
+        y: -200,
+        duration: 3,
+        backgroundColor: 'green',
+    }
+);
