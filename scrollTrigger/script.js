b03c89d6d85d8544444ce231e4c0369638e0bfe6
William Dias <smdwilliam89@gmail.com>
2025-04-29 00:20:46 -0300

    feat(script(scrollTrigger)): Brincando com animações.

--- scrollTrigger/script.js
+++ scrollTrigger/script.js
@@ -1,17 +1,56 @@
 gsap.registerPlugin(ScrollTrigger);
 
-gsap.from('.quadrado', {
-    opacity: 0,
-    y: 40,
-    x: 30,
-    rotate: 30,
-    filter: 'blur(20px)',
-    delay: 0.3,
+const quadrados = document.querySelectorAll('.quadrado');
+
+const tl = gsap.timeline();
+
+tl.to(quadrados[1], {
+    x: '110%',
     scrollTrigger: {
-        trigger: '.container',
-        start: '20% top',
-        end: '85% bottom',
-        markers: true,
+        trigger: '.quadrado',
+        start: '0%, 50%',
+        end: '100%, 80%',
         scrub: 2,
     },
-});
+})
+    .to(quadrados[2], {
+        x: '220%',
+        scrollTrigger: {
+            trigger: '.quadrado',
+            start: '0%, 50%',
+            end: '100%, 80%',
+            scrub: 2,
+        },
+    })
+    .to(quadrados[3], {
+        x: '330%',
+        scrollTrigger: {
+            trigger: '.quadrado',
+            start: '0%, 50%',
+            end: '100%, 80%',
+            scrub: 2,
+        },
+    })
+    .to(quadrados[4], {
+        x: '440%',
+        opacity: 0,
+        scrollTrigger: {
+            trigger: '.quadrado',
+            start: '0%, 50%',
+            end: '100%, 80%',
+            scrub: 2,
+        },
+    });
+
+// tl.to('.quadrado', {
+//     x: 100,
+//     rotate: 40,
+//     scrollTrigger: {
+//         trigger: '.quadrado',
+//         start: '-50% 50%',
+//         end: '+=600 50%',
+//         markers: true,
+//         scrub: 2,
+//         pin: true,
+//     },
+// });
